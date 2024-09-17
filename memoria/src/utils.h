1fcfaa449644d27235ed679994d84a06cf19cd9a
Alexander Juncos <alexanderljuncos@gmail.com>
2024-09-17 18:58:29 +0000

    me olvide de actualizar comentarios de desarrollo

--- memoria/src/utils.h
+++ memoria/src/utils.h
@@ -92,7 +92,7 @@ t_tcb_mem* iniciar_tcb(int pid, int tid, char* ruta_script);
 t_pcb_mem* iniciar_pcb(int pid, int tamanio, char* ruta_script_tid_0);
 bool cargar_contexto_ejecucion(int pid, int tid);
 bool actualizar_contexto_ejecucion(t_list* nuevo_pedido_raw); // EN DESARROLLO
-char* obtener_instruccion(int num_instruccion); // EN DESARROLLO 
+char* obtener_instruccion(int num_instruccion);
 
 // ==========================================================================
 // ====  Funciones Externas:  ===============================================
