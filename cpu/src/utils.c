6cd6d002eb860338964a74a2ab58c998ff5d12e8
Alexander Juncos <alexanderljuncos@gmail.com>
2024-10-05 23:14:11 +0000

    decode hecho a manito, mejor no lo miremos

--- cpu/src/utils.c
+++ cpu/src/utils.c
@@ -22,6 +22,100 @@ pthread_mutex_t mutex_interrupcion;
 // ====  Funciones Internas:  ===============================================
 // ==========================================================================
 
+t_list* decode (char* instruccion)
+{
+    char **arg = string_split(instruccion, " ");
+    t_list* parametros = list_create();
+    void* var_instruccion = malloc(sizeof(int));
+    *var_instruccion = DESCONOCIDA;
+    int num_arg = string_array_size(arg);
+
+    if (strcmp(arg[0], "SET") == 0 && num_arg == 2){
+        *var_instruccion = SET;
+    }
+    if (strcmp(arg[0], "READ_MEM") == 0 && num_arg == 2){
+        *var_instruccion = READ_MEM;
+    }
+    if (strcmp(arg[0], "WRITE_MEM") == 0 && num_arg == 2){
+        *var_instruccion = WRITE_MEM;
+    }
+    if (strcmp(arg[0], "SUM") == 0 && num_arg == 2){
+        *var_instruccion = SUM;
+    }
+    if (strcmp(arg[0], "SUB") == 0 && num_arg == 2){
+        *var_instruccion = SUB;
+    }
+    if (strcmp(arg[0], "JNZ") == 0 && num_arg == 2){
+        *var_instruccion = JNZ;
+    }
+    if (strcmp(arg[0], "LOG") == 0 && num_arg == 1){
+        *var_instruccion = LOG;
+    }
+    if (strcmp(arg[0], "DUMP_MEMORY") == 0 && num_arg == 0){
+        *var_instruccion = DUMP_MEMORY;
+    }
+    if (strcmp(arg[0], "IO") == 0 && num_arg == 1){
+        *var_instruccion = IO;
+    }
+    if (strcmp(arg[0], "PROCESS_CREATE") == 0 && num_arg == 3){
+        *var_instruccion = PROCESS_CREATE;
+    }
+    if (strcmp(arg[0], "THREAD_CREATE") == 0 && num_arg == 2){
+        *var_instruccion = THREAD_CREATE;
+    }
+    if (strcmp(arg[0], "THREAD_JOIN") == 0 && num_arg == 1){
+        *var_instruccion = THREAD_JOIN;
+    }
+    if (strcmp(arg[0], "THREAD_CANCEL") == 0 && num_arg == 1){
+        *var_instruccion = THREAD_CANCEL;
+    }
+    if (strcmp(arg[0], "MUTEX_CREATE") == 0 && num_arg == 1){
+        *var_instruccion = MUTEX_CREATE;
+    }
+    if (strcmp(arg[0], "MUTEX_LOCK") == 0 && num_arg == 1){
+        *var_instruccion = MUTEX_LOCK;
+    }
+    if (strcmp(arg[0], "MUTEX_UNLOCK") == 0 && num_arg == 1){
+        *var_instruccion = MUTEX_UNLOCK;
+    }
+    if (strcmp(arg[0], "THREAD_EXIT") == 0){
+        *var_instruccion = THREAD_EXIT;
+    }
+    if (strcmp(arg[0], "PROCESS_EXIT") == 0){
+        *var_instruccion = PROCESS_EXIT;
+    }
+
+    list_add(parametros, *(int*)var_instruccion);
+    
+    // si no se conoce la instruccion devuelvo
+    if (*(int*)var_instruccion == DESCONOCIDA)
+        return parametros;
+
+    // paso los parametros
+    for (int i = 1; i <= num_arg; i++)
+    {
+        list_add(parametros, arg[i]);
+    }
+    return parametros;    
+}
+
+void instruccion_set (t_list* param)
+{
+    // log_info(log_cpu_gral, "PID: %d - Ejecutando: %s - %s %s", reg.pid, arg[0], arg[1], arg[2]);
+
+	// void* registro = dictionary_get(diccionario, arg[1]);
+	// 	*(uint32_t*)registro = atoi(arg[2]);
+	// 	log_debug(log_cpu_gral, "Se hizo SET de %u en %s", *(uint32_t*)registro, arg[1]); // temporal. Sacar luego
+	// }
+}
+
+void instruccion_read_mem (t_list* param);
+void instruccion_write_mem (t_list* param);
+void instruccion_sum (t_list* param);
+void instruccion_sub (t_list* param);
+void instruccion_jnz (t_list* param);
+void instruccion_log (t_list* param);
+
 // ==========================================================================
 // ====  Funciones Externas:  ===============================================
 // ==========================================================================
